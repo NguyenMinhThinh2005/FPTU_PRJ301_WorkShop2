0d68bd09805cf3293f7a7db2be75baef212142e2
NguyenMinhThinh2005 <thinhnguyen100816@gmail.com>
2025-06-16 22:36:32 +0700

    suon project

---
+++ ShopManagement_WorkShop2/src/java/dto/User.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Thinh
+ */
+public class User {
+
+    private String fullName;
+    private String userID;
+    private String password;
+    private String roleID;
+
+    public User() {
+    }
+
+    public User(String fullName, String userID, String password, String roleID) {
+        this.fullName = fullName;
+        this.userID = userID;
+        this.password = password;
+        this.roleID = roleID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.userID);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return Objects.equals(this.userID, other.userID);
+    }
+    
+    
+}
